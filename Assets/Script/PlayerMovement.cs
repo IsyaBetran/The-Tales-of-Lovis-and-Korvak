9c4dc191241d34a183d8a76fb20981e798e8aee4
IsyaBetran <isyaab2@gmail.com>
2022-03-04 14:07:48 +0700

    minorUpdate

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,15 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D badan;
     Transform t;
-    private float fixedRotation = 0;
-    public float gravitasi;
+    public float tinggiLompat;
 
     [SerializeField]private float kecepatan;
 
     private bool diTanah;
     public Transform cekTanah;
-    public float cekKetinggian;
+    public float cekKejauhan;
     public LayerMask tanahnya;
+
+    public LayerMask tangga;
+    private bool naikTangga;
 
     private float lompat = 1;
 
@@ -23,35 +25,43 @@ public class PlayerMovement : MonoBehaviour
         t = transform;
     }
 
-    private void FixedUpdate(){
-        diTanah = Physics2D.OverlapCircle(cekTanah.position, cekKetinggian, tanahnya);
-
+    private void Update(){
         //berjalan
         var jalan = Input.GetAxis("Horizontal");
         badan.velocity = new Vector2(jalan * kecepatan, badan.velocity.y);
 
-        //fixed rotation (Biar gk muter-muter)
-        t.eulerAngles = new Vector3 (t.eulerAngles.x, t.eulerAngles.y, fixedRotation);
+        RaycastHit2D kena = Physics2D.Raycast(transform.position, Vector2.up, cekKejauhan, tangga);
+        if(kena.collider != null){
+            if(Input.GetKeyDown(KeyCode.UpArrow)){
+                naikTangga =  true;
+            }       
+        }else{
+            naikTangga = false;
+        }
 
+        if(naikTangga == true){
+            var naik = Input.GetAxis("Vertical");
+            badan.velocity = new Vector2(badan.position.x, naik * kecepatan);
+            badan.gravityScale = 0f;
+        }else{
+            badan.gravityScale = 2f;
+        }
+        
         //balik badan
         if(jalan > 0.01f){
             transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
         }else if(jalan < -0.01f){
             transform.localScale = new Vector3(-0.15f, 0.15f, 0.15f);
-        }
-
-        
-    }
-
-    public void Update(){
+        }  
+        diTanah = Physics2D.OverlapCircle(cekTanah.position, cekKejauhan, tanahnya);
         //Double Jump
         if(diTanah == true){
             lompat = 1;
         }
         //lompat
         if(Input.GetKeyDown(KeyCode.Space) && lompat > 0){
-            badan.velocity = new Vector2(badan.velocity.x, kecepatan);
+            badan.velocity = new Vector2(badan.velocity.x, tinggiLompat);
             lompat--;
-        }
+        }  
     }
 }
