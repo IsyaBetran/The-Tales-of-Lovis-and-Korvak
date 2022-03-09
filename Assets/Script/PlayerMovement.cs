0858fadacc97a2076362175b48ba8e72ae1bb804
Parasian14 <agustinus1408t@gmail.com>
2022-03-09 22:42:45 +0700

    change

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private float jalan;
     private float naik;
     public Rigidbody2D badan { get; private set; }
+=======
+    public Rigidbody2D badan;
+>>>>>>> Stashed changes
     Transform t;
     public float tinggiLompat;
 
@@ -21,11 +25,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]private PlayerInteraction playerIn;
     private Vector3 lastPosition;
 
-    private void Awake(){
+    private void Awake()
+    {
         badan = GetComponent<Rigidbody2D>();
         t = transform;
     }
 
+<<<<<<< Updated upstream
     private void Update(){    
         //berjalan
         jalan = Input.GetAxis("Horizontal");
@@ -51,6 +57,36 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space) && lompat > 0){
             badan.velocity = new Vector2(badan.velocity.x, tinggiLompat);
             lompat--;
+=======
+    public void Update(){
+        //Double Jump
+        if(diTanah == true){
+            lompat = 2;
+        }
+        //lompat
+        if(Input.GetKeyDown(KeyCode.Space) && lompat > 0){
+            badan.velocity = new Vector2(badan.velocity.x, kecepatan);
+            lompat--;
+        }
+
+        diTanah = Physics2D.OverlapCircle(cekTanah.position, cekKetinggian, tanahnya);
+
+        //berjalan
+        var jalan = Input.GetAxis("Horizontal");
+        badan.velocity = new Vector2(Input.GetAxis("Horizontal") * kecepatan, badan.velocity.y);
+
+        //fixed rotation (Biar gk muter-muter)
+        t.eulerAngles = new Vector3(t.eulerAngles.x, t.eulerAngles.y, fixedRotation);
+
+        //balik badan
+        if (jalan > 0.01f)
+        {
+            transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
+        }
+        else if (jalan < -0.01f)
+        {
+            transform.localScale = new Vector3(-0.15f, 0.15f, 0.15f);
+>>>>>>> Stashed changes
         }
         
         
